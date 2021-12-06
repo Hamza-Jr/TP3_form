c7648df9d7631a5dbc6a4a7836f840baf478cc3a
Hamza-Jr <92399369+Hamza-Jr@users.noreply.github.com>
2021-12-06 04:09:22 +0100

    Add files via upload

---
+++ TP3_Form/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace TP3_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnt_Add = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_Delet = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.ID_Box = new System.Windows.Forms.ComboBox();
+            this.prenom_label = new System.Windows.Forms.Label();
+            this.Adresse_label = new System.Windows.Forms.Label();
+            this.ID_label = new System.Windows.Forms.Label();
+            this.tele_label = new System.Windows.Forms.Label();
+            this.nom_label = new System.Windows.Forms.Label();
+            this.input_nom = new System.Windows.Forms.TextBox();
+            this.input_tele = new System.Windows.Forms.TextBox();
+            this.input_adresse = new System.Windows.Forms.TextBox();
+            this.input_prenom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bnt_Add
+            // 
+            this.bnt_Add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bnt_Add.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bnt_Add.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnt_Add.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.bnt_Add.Location = new System.Drawing.Point(584, 482);
+            this.bnt_Add.Name = "bnt_Add";
+            this.bnt_Add.Size = new System.Drawing.Size(151, 73);
+            this.bnt_Add.TabIndex = 0;
+            this.bnt_Add.Text = "Insert";
+            this.bnt_Add.UseVisualStyleBackColor = false;
+            this.bnt_Add.Click += new System.EventHandler(this.bnt_Add_Click);
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Edit.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Edit.Enabled = false;
+            this.btn_Edit.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Edit.Location = new System.Drawing.Point(419, 482);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(165, 73);
+            this.btn_Edit.TabIndex = 1;
+            this.btn_Edit.Text = "Edit";
+            this.btn_Edit.UseVisualStyleBackColor = false;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cancel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_cancel.Location = new System.Drawing.Point(103, 482);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(165, 73);
+            this.btn_cancel.TabIndex = 2;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_Delet
+            // 
+            this.btn_Delet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Delet.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Delet.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delet.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Delet.Location = new System.Drawing.Point(261, 482);
+            this.btn_Delet.Name = "btn_Delet";
+            this.btn_Delet.Size = new System.Drawing.Size(165, 73);
+            this.btn_Delet.TabIndex = 3;
+            this.btn_Delet.Text = "Delet";
+            this.btn_Delet.UseVisualStyleBackColor = false;
+            this.btn_Delet.Click += new System.EventHandler(this.btn_Delet_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Save.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Save.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Save.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Save.Location = new System.Drawing.Point(1027, 490);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(118, 65);
+            this.btn_Save.TabIndex = 4;
+            this.btn_Save.Text = "save";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // ID_Box
+            // 
+            this.ID_Box.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ID_Box.FormattingEnabled = true;
+            this.ID_Box.Location = new System.Drawing.Point(489, 63);
+            this.ID_Box.Name = "ID_Box";
+            this.ID_Box.Size = new System.Drawing.Size(393, 24);
+            this.ID_Box.TabIndex = 5;
+            this.ID_Box.SelectedIndexChanged += new System.EventHandler(this.ID_Box_SelectedIndexChanged);
+            // 
+            // prenom_label
+            // 
+            this.prenom_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.prenom_label.AutoSize = true;
+            this.prenom_label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.prenom_label.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prenom_label.Location = new System.Drawing.Point(360, 186);
+            this.prenom_label.Name = "prenom_label";
+            this.prenom_label.Size = new System.Drawing.Size(103, 24);
+            this.prenom_label.TabIndex = 9;
+            this.prenom_label.Text = "PRENOM";
+            // 
+            // Adresse_label
+            // 
+            this.Adresse_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Adresse_label.AutoSize = true;
+            this.Adresse_label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Adresse_label.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adresse_label.Location = new System.Drawing.Point(351, 249);
+            this.Adresse_label.Name = "Adresse_label";
+            this.Adresse_label.Size = new System.Drawing.Size(112, 24);
+            this.Adresse_label.TabIndex = 10;
+            this.Adresse_label.Text = "ADRESSE";
+            // 
+            // ID_label
+            // 
+            this.ID_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ID_label.AutoSize = true;
+            this.ID_label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ID_label.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID_label.Location = new System.Drawing.Point(429, 63);
+            this.ID_label.Name = "ID_label";
+            this.ID_label.Size = new System.Drawing.Size(34, 24);
+            this.ID_label.TabIndex = 11;
+            this.ID_label.Text = "ID";
+            // 
+            // tele_label
+            // 
+            this.tele_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tele_label.AutoSize = true;
+            this.tele_label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tele_label.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tele_label.Location = new System.Drawing.Point(396, 319);
+            this.tele_label.Name = "tele_label";
+            this.tele_label.Size = new System.Drawing.Size(67, 24);
+            this.tele_label.TabIndex = 12;
+            this.tele_label.Text = "TELE";
+            // 
+            // nom_label
+            // 
+            this.nom_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nom_label.AutoSize = true;
+            this.nom_label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.nom_label.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nom_label.Location = new System.Drawing.Point(404, 122);
+            this.nom_label.Name = "nom_label";
+            this.nom_label.Size = new System.Drawing.Size(59, 24);
+            this.nom_label.TabIndex = 13;
+            this.nom_label.Text = "NOM";
+            // 
+            // input_nom
+            // 
+            this.input_nom.Location = new System.Drawing.Point(488, 122);
+            this.input_nom.Multiline = true;
+            this.input_nom.Name = "input_nom";
+            this.input_nom.Size = new System.Drawing.Size(394, 24);
+            this.input_nom.TabIndex = 14;
+            this.input_nom.TextChanged += new System.EventHandler(this.input_nom_TextChanged);
+            // 
+            // input_tele
+            // 
+            this.input_tele.Location = new System.Drawing.Point(488, 319);
+            this.input_tele.Multiline = true;
+            this.input_tele.Name = "input_tele";
+            this.input_tele.Size = new System.Drawing.Size(394, 24);
+            this.input_tele.TabIndex = 15;
+            this.input_tele.TextChanged += new System.EventHandler(this.input_tele_TextChanged);
+            // 
+            // input_adresse
+            // 
+            this.input_adresse.Location = new System.Drawing.Point(488, 249);
+            this.input_adresse.Multiline = true;
+            this.input_adresse.Name = "input_adresse";
+            this.input_adresse.Size = new System.Drawing.Size(394, 24);
+            this.input_adresse.TabIndex = 16;
+            this.input_adresse.TextChanged += new System.EventHandler(this.input_adresse_TextChanged);
+            // 
+            // input_prenom
+            // 
+            this.input_prenom.Location = new System.Drawing.Point(488, 186);
+            this.input_prenom.Multiline = true;
+            this.input_prenom.Name = "input_prenom";
+            this.input_prenom.Size = new System.Drawing.Size(394, 24);
+            this.input_prenom.TabIndex = 17;
+            this.input_prenom.TextChanged += new System.EventHandler(this.input_prenom_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1173, 578);
+            this.Controls.Add(this.input_prenom);
+            this.Controls.Add(this.input_adresse);
+            this.Controls.Add(this.input_tele);
+            this.Controls.Add(this.input_nom);
+            this.Controls.Add(this.nom_label);
+            this.Controls.Add(this.tele_label);
+            this.Controls.Add(this.ID_label);
+            this.Controls.Add(this.Adresse_label);
+            this.Controls.Add(this.prenom_label);
+            this.Controls.Add(this.ID_Box);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Delet);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.bnt_Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt_Add;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_Delet;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.ComboBox ID_Box;
+        private System.Windows.Forms.Label prenom_label;
+        private System.Windows.Forms.Label Adresse_label;
+        private System.Windows.Forms.Label ID_label;
+        private System.Windows.Forms.Label tele_label;
+        private System.Windows.Forms.Label nom_label;
+        private System.Windows.Forms.TextBox input_nom;
+        private System.Windows.Forms.TextBox input_tele;
+        private System.Windows.Forms.TextBox input_adresse;
+        private System.Windows.Forms.TextBox input_prenom;
+    }
+}
+
---
+++ TP3_Form/Form1.cs
@@ -0,0 +1,305 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3_Form
+{
+    public partial class Form1 : Form
+    {
+        #region CONSTRUCTOR
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region button methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Vouler vous annuler l'evenement ?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Vider(this);
+               
+                
+                ID_Box.Enabled = false;
+                bnt_Add.Enabled = true;
+                btn_Save.Enabled = false;
+                btn_cancel.Enabled = false;
+                input_nom.Enabled = false;
+                input_prenom.Enabled = false;
+                input_adresse.Enabled = false;
+                input_tele.Enabled = false;
+            }
+        }
+
+        private void btn_Delet_Click(object sender, EventArgs e)
+        {
+            Supprimer();
+        }
+
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+            Modifier();
+        }
+
+        private void bnt_Add_Click(object sender, EventArgs e)
+        {
+            bnt_Add.Enabled = false;
+            btn_Save.Enabled = true;
+            btn_cancel.Enabled = true;
+            input_nom.Enabled = true;
+            input_prenom.Enabled = true;
+            input_adresse.Enabled = true;
+            input_tele.Enabled = true;
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            Ajouter();
+            dropdownlist.Items.Clear();
+            adapter.Fill(ds, "DATAUSER");
+            for (int i = 0; i < ds.Tables["DATAUSER"].Rows.Count; i++)
+            {
+                dropdownlist.Items.Add(ds.Tables["datauser"].Rows[i][0]);
+            }
+        }
+        #endregion
+
+        #region input user
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ID_Box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for(int i = 0; i < ds.Tables["DATAUSER"].Rows.Count; i++)
+            {
+                if (dropdownlist.SelectedItem.Equals(ds.Tables["datauser"].Rows[i][0]))
+                {
+                    nomInput.Text = ds.Tables["datauser"].Rows[i][1].ToString();
+                    prenomInput.Text = ds.Tables["datauser"].Rows[i][2].ToString();
+                    adresseInput.Text = ds.Tables["datauser"].Rows[i][3].ToString();
+                    break;
+                }
+            }
+            //SelectData();
+
+            AjouterBtn.Enabled = false;
+            ModifierBtn.Enabled = true;
+            SupprimerBtn.Enabled = true;
+            nomInput.Enabled = false;
+            prenomInput.Enabled = false;
+            adresseInput.Enabled = false;
+        }
+        private void input_nom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_prenom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_adresse_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_tele_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Methods de traitment
+        /// <summary>
+        ///  pour vider les champs utiliser method Vider
+        /// </summary>
+        /// <param name="test"></param>
+        public void Vider(Control test)
+        {
+            foreach (Control ct in test.Controls)
+            {
+                if (ct.GetType() == typeof(TextBox))
+                {
+                    ct.Text = "";
+                }
+                if (ct.Controls.Count != 0)
+                    Vider(ct);
+            }
+        }
+
+        /// <summary>
+        /// function pour confirmer les differents types des events
+        /// </summary>
+        private void Confirmer()
+        {
+            DialogResult confirm = MessageBox.Show("Confirm to close ?", "Confirmation", MessageBoxButtons.YesNo);
+            DialogResult message = MessageBox.Show("Donnees enregistrees", "Succes", MessageBoxButtons.OK);
+            if (confirm == DialogResult.Yes)
+            {
+                if (message == DialogResult.OK)
+                {
+                    Vider(this);
+                    ID_Box.Enabled = true;
+                    bnt_Add.Enabled = true;
+                    btn_Save.Enabled = false;
+                    btn_Delet.Enabled = false;
+                    btn_Edit.Enabled = false;
+                    btn_cancel.Enabled = false;
+                    input_nom.Enabled = false;
+                    input_prenom.Enabled = false;
+                    input_adresse.Enabled = false;
+                    input_tele.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Erreur", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        /// <summary>
+        /// Ajouter utilisateur avec verification de champs sont pas vide
+        /// </summary>
+        private void Ajouter()
+        {
+            if (input_nom.Text == "" || input_prenom.Text == "" || input_adresse.Text == "" || input_tele.Text == "")
+            {
+                MessageBox.Show("Voulez vous remplir les champs", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                //switch between func============================>>
+                UpdateData();
+                //InsertData();
+                Confirmer();
+
+            }
+        }
+
+        private void Modifier()
+        {
+            bnt_Add.Enabled = false;
+            btn_Delet.Enabled = false;
+            btn_Edit.Enabled = false;
+            btn_Save.Enabled = true;
+            btn_cancel.Enabled = true;
+            ID_Box.Enabled = false;
+            input_nom.Enabled = true;
+            input_prenom.Enabled = true;
+            input_adresse.Enabled = true;
+            input_tele.Enabled = true;
+        }
+
+        private void Supprimer()
+        {
+            ID_Box.Enabled = true;
+            bnt_Add.Enabled = false;
+            btn_Delet.Enabled = false;
+            btn_Save.Enabled = false;
+            btn_Edit.Enabled = false;
+            btn_cancel.Enabled = false;
+            input_nom.Enabled = false;
+            input_prenom.Enabled = false;
+            input_adresse.Enabled = false;
+            input_tele.Enabled = false;
+            DialogResult deletperson = MessageBox.Show("Vouler vous supprimer ?", "Confirmation", MessageBoxButtons.YesNo);
+            if (deletperson == DialogResult.Yes )
+            {
+                DeleteData();
+                //dropdownlist.Items.Remove(dropdownlist.SelectedItem);
+                bnt_Add.Enabled = true;
+                btn_Delet.Enabled = false;
+                btn_Save.Enabled = false;
+                btn_cancel.Enabled = false;
+                ID_Box.Enabled = true;
+                input_nom.Enabled = false;
+                input_prenom.Enabled = false;
+                input_tele.Enabled = false;
+            }
+
+
+        }
+
+
+
+        #endregion
+
+        #region SqlMethods
+        /// <summary>
+        /// inserer les donnees
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void InsertData()
+        {
+            try
+            {
+                cnx.Open();
+                OracleCommand cmd = new OracleCommand("insertUser", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("nom", OracleDbType.Varchar2).Value = nomInput.Text;
+                cmd.Parameters.Add("prenom", OracleDbType.Varchar2).Value = prenomInput.Text;
+                cmd.Parameters.Add("adresse", OracleDbType.Varchar2).Value = adresseInput.Text;
+                cmd.ExecuteNonQuery();
+                // MessageBox.Show("donnee inseree");        
+            }
+            catch
+            {
+                MessageBox.Show("ERREUR");
+            }
+            cnx.Close();
+        }
+        /// <summary>
+        /// modifier donnees
+        /// </summary>
+        public void UpdateData()
+        {
+            cnx.Open();
+            OracleCommand cmd = new OracleCommand("updateUser", cnx);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id_user", OracleDbType.Int32).Value = Convert.ToInt32(dropdownlist.SelectedItem);
+            cmd.Parameters.Add("nom", OracleDbType.Varchar2).Value = nomInput.Text;
+            cmd.Parameters.Add("prenom", OracleDbType.Varchar2).Value = prenomInput.Text;
+            cmd.Parameters.Add("adresse", OracleDbType.Varchar2).Value = adresseInput.Text;
+            cmd.ExecuteNonQuery();
+            //MessageBox.Show("donnee est modifiee");
+            cnx.Close();
+        }
+        /// <summary>
+        /// supprimer les donnees
+        /// </summary>
+        public void DeleteData()
+        {
+            cnx.Open();
+            OracleCommand cmd = new OracleCommand("deleteUser", cnx);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id_user", OracleDbType.Int32).Value = Convert.ToInt32(dropdownlist.SelectedItem);
+            cmd.ExecuteNonQuery();
+            //MessageBox.Show("person est supprime");
+            cnx.Close();
+        }
+        #endregion
+    }
+}
